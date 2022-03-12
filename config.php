9198cd783ce078c19d4e0d2de740bb5a9b71eede
Manpreet <manpreet.7385@gmail.com>
2022-03-12 23:45:05 +0530

    config updated

--- config.php
+++ config.php
@@ -1,5 +1,9 @@
 <?php
   include 'admin/php_files/database.php';
-  $hostname = "https://localhost/super-market"
-    
+  echo gethostname(); 
+  $myvar = gethostname();
+  $str1 = "https://";
+  $str2 = "/super-market";
+  $hostname = $str1 . ''. $myvar . ''. $str2;
+  echo $hostname;
 ?>
