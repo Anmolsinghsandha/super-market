cd239df7ea65e552ba8415e2ab1f16aa81ae7d0c
Manpreet <manpreet.7385@gmail.com>
2022-03-12 23:04:22 +0530

    super-market

---
+++ instamojo.php
@@ -0,0 +1,33 @@
+<?php 
+include 'config.php';
+
+session_start();
+$user = $_SESSION['username'];
+
+$db = new Database();
+$db->select('options','site_name',null,null,null,null);
+$site_name = $db->getResult();
+
+$_SESSION['TID'] = $response->payment_request->id;
+$params1 = [
+    'item_number' => $_POST['product_id'],
+    'txn_id' => $response->payment_request->id,
+    'payment_gross' => $_POST['product_total'],
+    'payment_status' => 'credit',
+];
+$params2 = [
+    'product_id' => $_POST['product_id'],
+    'product_qty' => $_POST['product_qty'],
+    'total_amount' => $_POST['product_total'],
+    'product_user' => $_SESSION['user_id'],
+    'order_date' => date('Y-m-d'),
+    'pay_req_id' => $response->payment_request->id
+];
+$db = new Database();
+$db->insert('payments',$params1);
+$db->insert('order_products',$params2);
+$db->getResult();
+
+header('Location: '. $hostname.'/success.php');
+
+?>
