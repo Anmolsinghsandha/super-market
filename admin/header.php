f95c9f7e973a2722dfd612749e880b629be9f33c
Manpreet <manpreet.7385@gmail.com>
2022-03-13 08:10:19 +0530

    header, index, config and logout admin

--- admin/header.php
+++ admin/header.php
@@ -2,7 +2,7 @@
     include 'php_files/config.php';
     if(!session_id()){ session_start(); }
     if(!isset($_SESSION['admin_name'])) {
-        header("location:{$base_url}/admin");
+        header("location: admin");
     }
 
     $db = new Database();
